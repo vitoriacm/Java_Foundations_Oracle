f1ed02e3b0920e343dd1e2c99347d017b9ced1aa
vitoriacm <anavitoria.cezar@gmail.com>
2023-06-03 21:48:38 -0300

    First Commit

---
+++ JFoPractice2/JFoPractice_2.java
@@ -0,0 +1,17 @@
+package JFoPractice2;
+
+public class JFoPractice_2 {
+    
+    public static void main(String[] args) {
+           
+    System.out.println ("      xxxx   ");  
+    System.out.println ("   x        x");
+    System.out.println ("  x   #  #   x"); 
+    System.out.println ("  x          x");
+    System.out.println ("  x   #  #   x"); 
+    System.out.println ("  x    ##    x"); 
+    System.out.println ("   x        x"); 
+    System.out.println ("      xxxx   ");
+    System.out.println                ("");
+            }         
+}
---
+++ JFoPractice2/Problema2.java
@@ -0,0 +1,14 @@
+package JFoPractice2;
+
+public class Problema2 {
+    
+public static void main(String[] args) {
+    
+System.out.println("   | |-----------------------| |   ");
+System.out.println("   | |-----------------------| |  ");
+System.out.println("   | |-----------------------| |");
+System.out.println("   | |                       | |   ");
+System.out.println("          Uma Cerca                    ");
+}
+
+}
